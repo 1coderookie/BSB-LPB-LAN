99ed4e14cf6acd3d04460ed01e50853560df5b82
U. D <1coderookie@quantentunnel.de>
2021-02-11 11:15:51 +0100

    Update pwabuilder-sw.js

--- docs/pwabuilder-sw.js
+++ docs/pwabuilder-sw.js
@@ -5,7 +5,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "https://1coderookie.github.io/BSB-LPB-LAN/index.html";
+const offlineFallbackPage = "index.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
