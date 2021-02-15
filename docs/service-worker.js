8fa5cfb55132e80308aeeaeace9275ac203f861d
U. D <1coderookie@quantentunnel.de>
2021-02-15 16:29:44 +0100

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,3 +1,4 @@
+
 // PWA Fire Bundle
                       
         // after a service worker is installed and the user navigates to a different page or 
@@ -27,35 +28,35 @@
         // add your entire site to the cache- as in the code below; for offline access
         // If you have some build process for your site, perhaps that could 
         // generate the list of possible URLs that a user might load.               
-               // '/', // do not remove this
-                '.', 
-               // '/index.html', //default
-                './index.html',
-                'inhaltsverzeichnis.html?homescreen=1', //default
-                '?homescreen=1', //default
-                'assets/css/style.css?v=1ae6ea59cc29b8a55941394bec5ba63fd5b09c72',// configure as by your site ; just an example
+                './', // do not remove this
+               // '.', 
+                './index.html', //default
+               // 'index.html',
+                './index.html?homescreen=1', //default
+                './?homescreen=1', //default
+                './assets/css/style.css*',// configure as by your site ; just an example
                 './kap01.html',
                 './kap02.html',
-                'kap03.html',
-                'kap04.html',
-                'kap05.html',
-                'kap06.html',
-                'kap07.html',
-                'kap08.html',
-                'kap09.html',
-                'kap10.html',
-                'kap11.html',
-                'kap12.html',
-                'kap13.html',
-                'kap14.html',
-                'kap15.html',
-                'kap16.html',
-                'kap17.html',
-                'anhang_a1.html',
-                'anhang_a2.html',
-                'anhang_b.html',
-                'anhang_c.html',
-                'anhang_d.html',
+                './kap03.html',
+                './kap04.html',
+                './kap05.html',
+                './kap06.html',
+                './kap07.html',
+                './kap08.html',
+                './kap09.html',
+                './kap10.html',
+                './kap11.html',
+                './kap12.html',
+                './kap13.html',
+                './kap14.html',
+                './kap15.html',
+                './kap16.html',
+                './kap17.html',
+                './anhang_a1.html',
+                './anhang_a2.html',
+                './anhang_b.html',
+                './anhang_c.html',
+                './anhang_d.html',
                 './pics/*.{png,jpg,jpeg}',// choose images to keep offline; just an example
         // Do not replace/delete/edit the manifest.js paths below
         //These are links to the extenal social media buttons that should be cached;
