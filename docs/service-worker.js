9fecdcd844b8f8b80504387c22cea347fe110135
U. D <1coderookie@quantentunnel.de>
2021-02-15 16:51:18 +0100

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -34,7 +34,9 @@
                // 'index.html',
                 './index.html?homescreen=1', //default
                 './?homescreen=1', //default
-                './assets/css/style.css*',// configure as by your site ; just an example
+               // './assets/css/style.css*',// configure as by your site ; just an example
+                './index2.html',
+                './inhaltsverzeichnis.html',
                 './kap01.html',
                 './kap02.html',
                 './kap03.html',
